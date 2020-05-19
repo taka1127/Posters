21257dcb5a701c5ad54fc97235977726159658ca
taka1127 <taka07251127@gmail.com>
2020-05-19 15:29:31 +0900

    投稿処理のコントローラ

---
+++ src/servlet/NewServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/NewServlet")
+public class NewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public NewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/new.jsp");
+        dispatcher.forward(request,response);
+        System.out.println("-----表示中の画面-----");
+        System.out.println("/new");
+        System.out.println("");
+	}
+
+}
